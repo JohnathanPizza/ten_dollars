6a058bfb66c62f70ecb5f9a6d90794292af7d514
Connor Bremner <connorbremner@mines.edu>
2024-07-20 22:18:41 -0600

    tasm.sh accepts crab flags

--- tasm.sh
+++ tasm.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-crab -b0x8000 -s0x8000 `cat filelist`
+if [ $# -eq 0 ]; then
+	crab -b0x8000 -s0x8000 `cat filelist`
+elif [ $# -eq 1 ]; then
+	crab "$1" -b0x8000 -s0x8000 `cat filelist`
+fi
